bda3df77b0c07140316c5d6616403c992a49f4cb
Morgan Thowsen <thowsenm@gmail.com>
2019-09-17 12:52:34 +0200

    test comment

--- app/src/main/java/com/dit212/group1/koskenkiosken/MainActivity.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * test comment
+     * @param savedInstanceState tjohej
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
