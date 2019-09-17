9a9229a293cb43f4d64820b264c094aca5aff805
Morgan Thowsen <thowsenm@gmail.com>
2019-09-17 15:41:34 +0200

    testing Travis tripwire with bad test.

--- app/src/test/java/com/dit212/group1/koskenkiosken/ExampleUnitTest.java
+++ app/src/test/java/com/dit212/group1/koskenkiosken/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void thisTestShouldBeRejected(){
+        assertEquals(0,1);
+    }
 }
