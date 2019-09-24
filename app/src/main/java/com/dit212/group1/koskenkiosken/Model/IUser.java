85bb825040aad806facee79b1e72d23b748a08b3
Morgan Thowsen <thowsenm@gmail.com>
2019-09-24 08:43:12 +0200

    abstracted user to a IUser interface and streamlined the user representation. (passwords and email can be added at a later point)

---
+++ app/src/main/java/com/dit212/group1/koskenkiosken/Model/IUser.java
@@ -0,0 +1,11 @@
+package com.dit212.group1.koskenkiosken.Model;
+
+/**
+ * Author: created by thowsen, 2019-09-24
+ * Description:
+ */
+public interface IUser {
+
+    String getUserName();
+    int getCredits();
+}
