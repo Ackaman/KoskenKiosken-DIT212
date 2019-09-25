1eaea0ef5b898fcff0696045a2fb032d998062d4
Ackaman <johan_a_94@hotmail.com>
2019-09-25 15:23:28 +0200

    Placeholder class ProductPressedView

---
+++ app/src/main/java/com/dit212/group1/koskenkiosken/ProductPressedView.java
@@ -0,0 +1,14 @@
+package com.dit212.group1.koskenkiosken;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductPressedView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
