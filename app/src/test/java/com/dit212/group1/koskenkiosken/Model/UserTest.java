83cfd356ba8a83f9c12517c8a92268db30b42eef
Albin Otterhäll <gusalbiot@student.gu.se>
2019-09-26 23:46:28 +0200

    Add test for `User` directly

---
+++ app/src/test/java/com/dit212/group1/koskenkiosken/Model/UserTest.java
@@ -0,0 +1,30 @@
+package com.dit212.group1.koskenkiosken.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+/**
+ * Primary author: Albin Otterhäll <gusalbiot@student.gu.se>
+ */
+
+public class UserTest {
+    private IAccount user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User("John Smith", 100);
+    }
+
+    @Test
+    public void nameIsCorrect() {
+        assertThat(user.getUserName(), equalTo("John Smith"));
+    }
+
+    @Test
+    public void getCredits() {
+        assertThat(user.getCredits(), equalTo(100));
+    }
+}
