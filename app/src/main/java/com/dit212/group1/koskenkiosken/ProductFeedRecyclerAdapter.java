a70f7b7f3bd709d3d7f512404297952cf5d51899
Morgan Thowsen <thowsenm@gmail.com>
2019-09-26 23:19:38 +0200

    replaced old cart button with new imagebutton more suitable.

--- app/src/main/java/com/dit212/group1/koskenkiosken/ProductFeedRecyclerAdapter.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/ProductFeedRecyclerAdapter.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -101,7 +101,7 @@ public class ProductFeedRecyclerAdapter extends RecyclerView.Adapter<ProductFeed
 
         TextView productName;
         TextView productPrice;
-        Button addToCart_button;
+        ImageButton addToCart_button;
 
         ProductClickListener productClickListener;
         PurchaseClickListener purchaseClickListener;
