bd8884b68ab182edb87ceb361c0c9291811a23d6
Albin Otterhäll <gusalbiot@student.gu.se>
2019-09-26 19:28:36 +0200

    Add test to check mock user's name

---
+++ app/src/test/java/com/dit212/group1/koskenkiosken/MockUserFactoryTest.java
@@ -0,0 +1,25 @@
+package com.dit212.group1.koskenkiosken;
+
+import com.dit212.group1.koskenkiosken.Model.IUser;
+import com.dit212.group1.koskenkiosken.Model.UserFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MockUserFactoryTest {
+    private IUser mockUser;
+
+    @Before
+    public void setUp() throws Exception {
+        mockUser = UserFactory.createMock();
+    }
+
+    @Test
+    public void defaultNameIsCorrect() {
+        String mockUserName = mockUser.getUserName();
+        assertThat(mockUserName, equalTo("FirstUser"));
+    }
+}
