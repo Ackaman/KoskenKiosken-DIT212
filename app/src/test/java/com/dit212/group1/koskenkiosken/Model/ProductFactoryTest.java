838d851268f42224fdbcd69e51e8d7275b8f0357
Albin Otterhäll <gusalbiot@student.gu.se>
2019-09-29 15:00:37 +0200

    Add tests for the product factory

---
+++ app/src/test/java/com/dit212/group1/koskenkiosken/Model/ProductFactoryTest.java
@@ -0,0 +1,27 @@
+package com.dit212.group1.koskenkiosken.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+public class ProductFactoryTest {
+
+    IProduct product;
+
+    @Before
+    public void setUp() throws Exception {
+        product = ProductFactory.create("Product 1", 50);
+    }
+
+    @Test
+    public void getName() {
+        assertThat(product.getName(), equalTo("Product 1"));
+    }
+
+    @Test
+    public void getPrice() {
+        assertThat(product.getPrice(), equalTo(50));
+    }
+}
