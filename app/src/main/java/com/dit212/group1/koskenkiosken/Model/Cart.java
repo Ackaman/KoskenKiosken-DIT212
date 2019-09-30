be768dc57401a5051bb212e266c52337fd8a4f6c
thowsen <thowsenm@gmail.com>
2019-09-30 17:56:57 +0200

    commented all of cart class.

--- app/src/main/java/com/dit212/group1/koskenkiosken/Model/Cart.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/Model/Cart.java
@@ -6,34 +6,64 @@ import java.util.List;
 public class Cart implements ICart {
     private ArrayList<IProduct> products;
 
+    /**
+     * constructor
+     */
     Cart(){
         products = new ArrayList<>();
     }
 
+    /**
+     * adds an item to the cart.
+     * @param product the product to add to the cart.
+     */
     @Override
     public void addToCart(IProduct product) {
         products.add(product);
     }
 
+    /**
+     * removes a product from the cart.
+     * @param product the product to remove
+     */
     @Override
     public void removeFromCart(IProduct product) {
-
+        products.remove(product);
     }
 
+    /**
+     * replaces the old cart list with a new one.
+     */
     @Override
     public void emptyCart() {
+        products = new ArrayList<>();
     }
 
+    /**
+     * sums the price of all items in cart.
+     * @return the sum of all item prices in cart.
+     */
     @Override
     public int getPrice() {
-        return 0;
+        int sum = 0;
+        for (IProduct p : products)
+            sum = sum + p.getPrice();
+        return sum;
     }
 
+    /**
+     * returns the list of all IProducts (interface for incapsulation)
+     * @return the list of all products in cart.
+     */
     @Override
     public List<IProduct> viewCart() {
         return products;
     }
 
+    /**
+     * replaces the list of products in the cart with a new list.
+     * @param products the products in cart.
+     */
     public void setCart(List<IProduct> products) {
         this.products = new ArrayList<>(products);
     }
