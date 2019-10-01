7b09ea9cf26f03f1b60385ffa9099c8fe98bea32
thowsen <thowsenm@gmail.com>
2019-10-01 16:02:21 +0200

    done implemententing comparator. untested.

--- app/src/main/java/com/dit212/group1/koskenkiosken/Model/ComparatorIProduct.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/Model/ComparatorIProduct.java
@@ -1,16 +1,13 @@
 package com.dit212.group1.koskenkiosken.Model;
 
-import android.util.Log;
-
 import java.util.Comparator;
 
 /**
  * Created by morgan on 2019-09-30
- * <p>
- * TODO , add comment
+ *
+ * Description: class provides predefined product comparators.
  */
 public class ComparatorIProduct {
-    private static String LOG_COMPARATORIPRODUCT = "COMP-IPRODUCT";
 
     private static Comparator<IProduct> nameAscendingOrder;
     private static Comparator<IProduct> nameDescendingOrder;
@@ -23,54 +20,25 @@ public class ComparatorIProduct {
     private ComparatorIProduct() {
     }
 
-    /**
-     * returns a IProduct comparator comparing names in ascending order.
-     * @return returns a IProduct comparator comparing names.
-     */
-
     public static Comparator<IProduct> nameAscendingOrder() {
         if (nameAscendingOrder != null) return nameAscendingOrder;
         nameAscendingOrder = new Comparator<IProduct>() {
             @Override
             public int compare(IProduct p1, IProduct p2) {
                 {
-                    if (p1.getName().equals(p2.getName())) {
-                        Log.i(LOG_COMPARATORIPRODUCT, p1.toString() + " is equal to " + p2.toString());
-                        return 0;
-                    }
-                    if (stringIsAfterInOrder(p1.toString(), p2.toString())) {
-                        Log.i(LOG_COMPARATORIPRODUCT, p1.toString() + " is after " + p2.toString());
-                        return 1;
-                    } else {
-                        Log.i(LOG_COMPARATORIPRODUCT, p1.toString() + " is before " + p2.toString());
-                        return -1;
-                    }
+                    return p2.getName().compareTo(p1.getName());
                 }
             }
         };
         return nameAscendingOrder;
     }
 
-    /**
-     * returns a IProduct comparator comparing names in decending order.
-     * @return returns a IProduct comparator comparing names.
-     */
-
     public static Comparator<IProduct> nameDescendingOrder() {
         if (nameDescendingOrder != null) return nameDescendingOrder;
         nameDescendingOrder = new Comparator<IProduct>() {
             @Override
             public int compare(IProduct p1, IProduct p2) {
-                {
-                    if (p1.getName().equals(p2.getName())) {
-                        return 0;
-                    }
-                    if (stringIsAfterInOrder(p1.toString(), p2.toString())) {
-                        return -1;
-                    } else {
-                        return 1;
-                    }
-                }
+                return p1.getName().compareTo(p2.getName());
             }
         };
         return nameDescendingOrder;
@@ -81,16 +49,7 @@ public class ComparatorIProduct {
         priceDescendingOrder = new Comparator<IProduct>() {
             @Override
             public int compare(IProduct p1, IProduct p2) {
-                {
-                    if (p1.getPrice() == p2.getPrice()) {
-                        return 0;
-                    }
-                    if (p1.getPrice() < p2.getPrice()) {
-                        return -1;
-                    } else {
-                        return 1;
-                    }
-                }
+                return Integer.valueOf(p1.getPrice()).compareTo(p2.getPrice());
             }
         };
         return nameDescendingOrder;
@@ -102,41 +61,14 @@ public class ComparatorIProduct {
             @Override
             public int compare(IProduct p1, IProduct p2) {
                 {
-                    if (p1.getPrice() == p2.getPrice()) {
-                        return 0;
-                    }
-                    if (p1.getPrice() < p2.getPrice()) {
-                        return 1;
-                    } else {
-                        return -1;
-                    }
+                    return Integer.valueOf(p2.getPrice()).compareTo(p1.getPrice());
                 }
             }
         };
         return nameAscendingOrder;
     }
-
-    /**
-     * checks if string1 is after string2 in alphabetical order.
-     * stringIsAfterInOrder(Horsepower, Horse) would be true.
-     *
-     * obs. we dont have access to Comparator static methods due to API level.
-     *
-     * @param s1 the first string
-     * @param s2 the second string.
-     * @return true if first string comes after second string in alphabetical order.
-     */
-    public static boolean stringIsAfterInOrder(String s1, String s2) {
-        char[] string1 = s1.toCharArray();
-        char[] string2 = s2.toCharArray();
-        int size = s1.length() < s2.length() ? s1.length() : s2.length();
-        for (int i = 0; i < size; i++) {
-            if (string1[i] != string2[i]) {
-                return string1[i] > string2[i];
-            }
-        }
-        return s1.length() > s2.length();
-    }
-
-
 }
+
+
+
+
--- app/src/test/java/com/dit212/group1/koskenkiosken/Model/ComparatorIProductTest.java
+++ app/src/test/java/com/dit212/group1/koskenkiosken/Model/ComparatorIProductTest.java
@@ -1,6 +1,14 @@
 package com.dit212.group1.koskenkiosken.Model;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -11,8 +19,40 @@ import static org.junit.Assert.*;
  */
 public class ComparatorIProductTest {
 
+    private List<IProduct> products;
+
+    @Before
+    public void init(){
+        products = new ArrayList<>();
+        IProduct p1mock = Mockito.mock(IProduct.class);
+        Mockito.when(p1mock.getName()).thenReturn("Äpple");
+        Mockito.when(p1mock.getPrice()).thenReturn(5);
+
+
+        IProduct p2mock = Mockito.mock(IProduct.class);
+        Mockito.when(p1mock.getName()).thenReturn("Päron");
+        Mockito.when(p1mock.getPrice()).thenReturn(8);
+
+
+        IProduct p3mock = Mockito.mock(IProduct.class);
+        Mockito.when(p1mock.getName()).thenReturn("Kitkat");
+        Mockito.when(p1mock.getPrice()).thenReturn(15);
+
+
+        IProduct p4mock = Mockito.mock(IProduct.class);
+        Mockito.when(p1mock.getName()).thenReturn("Kaffe");
+        Mockito.when(p1mock.getPrice()).thenReturn(2);
+
+        products.add(p1mock);
+        products.add(p2mock);
+        products.add(p3mock);
+        products.add(p4mock);
+    }
+
     @Test
     public void nameAscendingOrder() {
+        Comparator<IProduct> comp1 = ComparatorIProduct.nameAscendingOrder();
+        assertTrue(comp1.compare(products.get(0), products.get(0)) == 0);
     }
 
     @Test
@@ -31,13 +71,11 @@ public class ComparatorIProductTest {
     public void stringIsHejisAfterDaInOrder() {
         String str1 = "hej";
         String str2 = "då";
-        assertTrue(ComparatorIProduct.stringIsAfterInOrder(str1,str2));
     }
 
     @Test
     public void emptyStringisBeforeHejInOrder() {
         String str1 = "hej";
         String str2 = "";
-        assertTrue(ComparatorIProduct.stringIsAfterInOrder(str1,str2));
     }
 }
