db24384644a76cb160e69729e5e1e2bebe6fa3fd
thowsen <thowsenm@gmail.com>
2019-10-01 17:05:47 +0200

    added model test stump

---
+++ app/src/test/java/com/dit212/group1/koskenkiosken/Model/ModelTest.java
@@ -0,0 +1,37 @@
+package com.dit212.group1.koskenkiosken.Model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by morgan on 2019-10-01
+ * <p>
+ * TODO , add comment
+ */
+public class ModelTest {
+
+    @Test
+    public void listOfProducts() {
+    }
+
+    @Test
+    public void getLoggedInUser() {
+    }
+
+    @Test
+    public void getCart() {
+    }
+
+    @Test
+    public void addToCart() {
+    }
+
+    @Test
+    public void filterListByString() {
+    }
+
+    @Test
+    public void sortProducts() {
+    }
+}
