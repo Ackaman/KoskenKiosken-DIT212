280436240a4621e5b9236a124b0ec2d594ce1d16
thowsen <thowsenm@gmail.com>
2019-10-11 18:19:51 +0200

    removed productdependency from JSONparser

--- app/src/main/java/com/dit212/group1/koskenkiosken/DB/JSONparser.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/DB/JSONparser.java
@@ -1,11 +1,5 @@
 package com.dit212.group1.koskenkiosken.DB;
 
-import com.dit212.group1.koskenkiosken.Model.Product.IProduct;
-import com.dit212.group1.koskenkiosken.Model.Product.ProductFactory;
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-
 
 /**
  * Created by morgan on 2019-10-07
@@ -21,19 +15,22 @@ public class JSONparser implements IPersistentStorage{
      * hardcoded values because we don't know how to use an API.
      */
     JSONparser(){
-        ArrayList<IProduct> productsList = new ArrayList<>();
-        productsList.add(ProductFactory.create("Chokladboll", 2, "description 1"));
-        productsList.add(ProductFactory.create("Nocco", 1,"description 2"));
-        productsList.add(ProductFactory.create("HariboNallar", 3,"description 3"));
-        productsList.add(ProductFactory.create("Kaffepaket", 4,"description 4"));
-
-        Gson g = new Gson();
-        json = g.toJson(productsList.toArray(), IProduct[].class);
+        json = "[{\"description\":\"description 1\",\"name\":\"Chokladboll\",\"price\":2}," +
+                "{\"description\":\"description 2\",\"name\":\"Nocco\",\"price\":1}," +
+                "{\"description\":\"description 3\",\"name\":\"HariboNallar\",\"price\":3}," +
+                "{\"description\":\"description 4\",\"name\":\"Kaffepaket\",\"price\":4}," +
+                "{\"description\":\"description 4\",\"name\":\"Öl\",\"price\":15}," +
+                "{\"description\":\"description 4\",\"name\":\"Kaffe\",\"price\":2}," +
+                "{\"description\":\"description 4\",\"name\":\"Tuggummi\",\"price\":8}," +
+                "{\"description\":\"description 4\",\"name\":\"Ballerina\",\"price\":15}," +
+                "{\"description\":\"description 4\",\"name\":\"Singoalla\",\"price\":9}," +
+                "{\"description\":\"description 4\",\"name\":\"Naja\",\"price\":15}," +
+                "{\"description\":\"description 4\",\"name\":\"Cola\",\"price\":10}]";
     }
 
     /**
      * returns Json string
-     * @return
+     * @return json string containing data.
      */
     @Override
     public String getJsonData() {
