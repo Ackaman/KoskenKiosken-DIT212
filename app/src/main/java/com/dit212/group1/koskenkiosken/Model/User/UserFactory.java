7077e4b0a1375e0af6f0ef5aa81df11a4adc24fd
Albin Otterhäll <gusalbiot@student.gu.se>
2019-10-12 15:27:47 +0200

    Update method documentation

--- app/src/main/java/com/dit212/group1/koskenkiosken/Model/User/UserFactory.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/Model/User/UserFactory.java
@@ -9,18 +9,21 @@ package com.dit212.group1.koskenkiosken.Model.User;
 public class UserFactory implements IAccountFactory {
 
     /**
-     * factory method to create IAccount
-     * @param name the name to give the constructed user.
-     * @param credits the number of credits to give the constructed user.
-     * @return the constructed user.
+     * Factory method for `User` objects
+     *
+     * @param name The account's username.
+     * @param credits The amount of credits the `User` should have at creation.
+     *
+     * @return A `User` object
      */
     public static IAccount create(String name, int credits){
         return new User(name, credits);
     }
 
     /**
-     * factory method to create mock IAccount
-     * @return a constructed mockUser.
+     * Factory for a user account with predefined values.
+     *
+     * @return a `User` object with predefined values.
      */
 
     public static IAccount createMockUser(){
