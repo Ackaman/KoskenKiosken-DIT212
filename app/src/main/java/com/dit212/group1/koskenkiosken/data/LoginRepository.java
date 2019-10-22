152938ca00c93ccf4b229a57476a75ddf1694ae3
Albin Otterhäll <gusalbiot@student.gu.se>
2019-10-22 20:33:19 +0200

    Add Javadoc

--- app/src/main/java/com/dit212/group1/koskenkiosken/data/LoginRepository.java
+++ app/src/main/java/com/dit212/group1/koskenkiosken/data/LoginRepository.java
@@ -8,15 +8,26 @@ import com.dit212.group1.koskenkiosken.ui.login.LoginViewModel;
 import java.io.IOException;
 
 /**
+ * @author Albin Otterhäll <gusalbiot@student.gu.se>
+ *
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
  */
 public class LoginRepository {
 
+    /**
+     * The repository holding the state whether the user is authenticated or not.
+     */
     private static volatile LoginRepository instance;
 
+    /**
+     * The methods that does the actual authentication based on username and password
+     */
     private LoginDataSource dataSource;
 
+    /**
+     * A reference to the logged in user.
+     */
     // If user credentials will be cached in local storage, it is recommended it be encrypted
     // @see https://developer.android.com/training/articles/keystore
     private LoggedInUser user = null;
